887509cdb0d50a2349032c08452220d81881c486
Eric Loudon <loudon.eric@gmail.com>
2019-08-29 14:03:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,6 @@ extend Memorable
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
   end
@@ -23,8 +19,3 @@ extend Memorable
   def artist=(artist)
     @artist = artist
   end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
